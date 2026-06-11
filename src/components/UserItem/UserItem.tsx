ae051cff6afd3f94e323e8d9eca97eecd5a64d61
Johnny <espgoodlife22@gmail.com>
2026-06-11 15:10:19 +0300

    remove comments

--- src/components/UserItem/UserItem.tsx
+++ src/components/UserItem/UserItem.tsx
@@ -1,30 +1,3 @@
-// import { memo, useState } from 'react';
-// import type { UserType } from '../../types/types';
-// import css from './UserItem.module.css';
-// interface Props {
-//   item: UserType;
-//   toggle30Plus: boolean;
-// }
-
-// export const UserItem = memo(({ item, toggle30Plus }: Props) => {
-//   const [isActive, setIsActive] = useState(false);
-//   const onUserClick = () => {
-//     setIsActive(prev => !prev);
-//   };
-
-//   const highlited = isActive || (toggle30Plus && item.age >= 30);
-//   return (
-//     <div
-//       className={`${css.cardContainer} ${highlited ? css.active : css.notActive}`}
-//       onClick={() => onUserClick()}
-//     >
-//       <h3>Name: {item.name}</h3>
-//       <p>Id: {item.id}</p>
-//       <p>Age: {item.age}</p>
-//     </div>
-//   );
-// });
-
 import { memo } from 'react';
 import type { UserType } from '../../types/types';
 import css from './UserItem.module.css';
@@ -37,9 +10,10 @@ interface Props {
 
 export const UserItem = memo(
   ({ item, isActive, onUserClick, toggle30Plus }: Props) => {
-    // console.log(`Render user item ${item.name}`);
     const isHiglited = isActive || (toggle30Plus && item.age >= 30);
+
     console.log('render user the item component');
+
     return (
       <div
         className={`${css.cardContainer} ${isHiglited ? css.active : css.notActive}`}
